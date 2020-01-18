2b516232bb68d79928885565e2948110a6e81d3b
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2020-01-18 19:47:10 -0300

    Create Authors model and command to populate its table

---
+++ library_project/books_app/management/commands/populate_authors.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from books_app.models import Author
+import csv
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument("csv_entry", help="CSV file containing authors", type=open)
+        
+    def handle(self, *args, **options):
+        self.stdout.write("Init Authors Creation")
+        csv_file = options.get('csv_entry')
+        csv_content = csv.DictReader(csv_file)
+        try:
+            authors = (Author(name=author_row['name'].strip()) for author_row in csv_content)
+        except:
+            self.stderr.write("Error on document format.")
+        
+        batch = list(authors)
+        res = Author.objects.bulk_create(batch, ignore_conflicts=True)
+        self.stdout.write("Finish authors creation")
--- library_project/books_app/models.py
+++ library_project/books_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    """
+        Class representing authors of books
+    """
+    name = models.CharField(max_length=50, unique=True)
