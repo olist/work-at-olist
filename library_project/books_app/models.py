c06c4d7b10544c0e47d82d053ffe633858668f3f
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2020-01-18 21:16:17 -0300

    Add book model

--- library_project/books_app/models.py
+++ library_project/books_app/models.py
@@ -6,3 +6,13 @@ class Author(models.Model):
         Class representing authors of books
     """
     name = models.CharField(max_length=50, unique=True)
+
+class Book(models.Model):
+    """
+        Class representing books.
+        Books can have many authors, and authors can have many books
+    """
+    name = models.CharField(max_length=100) 
+    publication_year = models.PositiveIntegerField()
+    edition = models.PositiveIntegerField()
+    authors = models.ManyToManyField(Author, related_name='books')
--- library_project/books_app/views.py
+++ library_project/books_app/views.py
@@ -6,9 +6,12 @@ from books_app.models import Author
 from books_app.serializers import AuthorSerializer
 from books_app.filters import AuthorFilter
 
-# Create your views here.
 class AuthorViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    """
+        Viewset to retrieve Author data, only list and retrieve operations allowed.
+    """
     queryset = Author.objects.all().order_by('id')
     serializer_class = AuthorSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = AuthorFilter
+
