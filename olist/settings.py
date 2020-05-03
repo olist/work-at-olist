bbf1af8dff105207a35536e31fc0beb9e8b76216
jonatasilva <jonatas.iw@gmail.com>
2020-05-02 23:29:01 -0300

    adicionar .env com python decouple

--- olist/settings.py
+++ olist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd=uc=rhohlw%whm)x^1s5o8uhd5$oy4ykz9#r9yq@e=%qw1r+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
