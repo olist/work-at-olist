c302ddd7121f541d2527875a0982babe22c3cd90
gustavo <contato@gustavohalm.com>
2020-07-05 12:01:24 -0300

    Author View

--- library/views.py
+++ library/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import  viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .serializers import AuthorSerializer
+from .models import Author
 
-# Create your views here.
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
+    filterset_fields = ('name',)
